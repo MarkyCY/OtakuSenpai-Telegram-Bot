ce933ca34c8235b775f70c81530d89ee9ef8f8f3
Marcos Jesus <oranmarcos@gmail.com>
2023-07-19 18:35:38 -0400

    Add List of Admins command

--- main.py
+++ main.py
@@ -100,6 +100,23 @@ def start_ban_user(message):
     ban_user(message)
 
 
+@bot.message_handler(commands=['list_admins'])
+def list_admins(message):
+    # obtén la información del chat
+    chat_id = message.chat.id
+    chat_info = bot.get_chat(chat_id)
+    # obtén la lista de administradores del chat
+    admins = bot.get_chat_administrators(chat_id)
+    # crea una lista vacía para los nombres de los administradores (ignorando los bots)
+    admin_names = []
+    # itera sobre la lista de administradores y agrega los nombres de los que no son bots a la lista
+    for admin in admins:
+        if not admin.user.is_bot:
+            admin_names.append(admin.user)
+    # envía un mensaje con la lista de administradores al chat
+    bot.send_message(chat_id, f"Los administradores de {chat_info.title} son:\n" + "\n".join([f'<a href="https://t.me/{user.username}">{user.first_name}</a>' for user in admin_names]), parse_mode='html', disable_web_page_preview=True)
+    
+
 @bot.message_handler(commands=['unban'])
 def start_unban_user(message):
     unban_user(message)
