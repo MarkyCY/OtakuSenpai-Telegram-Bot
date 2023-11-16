dc6643dab49258d83180f0e73280e96b44dd8cac
Marcos J. Cardenas P <oranmarcos@gmail.com>
2023-11-16 01:38:48 -0500

    Create Event

---
+++ func/event.py
@@ -0,0 +1,30 @@
+import telebot
+import datetime
+import os
+
+from telebot.apihelper import ApiTelegramException
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+from pymongo import MongoClient
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Conectar a la base de datos
+client = MongoClient('localhost', 27017)
+db = client.otakusenpai
+users = db.users
+
+# Importamos los datos necesarios para el bot
+Token = os.getenv('BOT_API')
+bot = telebot.TeleBot(Token)
+
+
+
+def calvicia(uid, seconds):
+    try:
+        bot.send_message(uid, f"Ha(n) pasado {seconds} segundo(s)")
+    except ApiTelegramException as err:
+        print(err)
