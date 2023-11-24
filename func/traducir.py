c0b37cd19a9f203e65acadcfae198d3d87291765
Marcos J. Cardenas P <oranmarcos@gmail.com>
2023-11-24 01:00:47 -0500

    Delete unused translate function

--- func/traducir.py
+++
@@ -1,7 +0,0 @@
-from translate import Translator
-
-def translate(q):
-    text = q[:499]
-    translator = Translator(to_lang="es")
-    translation = translator.translate(f"{text}...")
-    return translation
