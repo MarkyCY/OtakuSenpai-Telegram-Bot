03213d264ed56e8a65577cc928c02c5e507357ec
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-01-23 02:25:04 -0500

    Add useControl Class

---
+++ func/useControl.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+import datetime
+
+class useControlMongo:
+    def __init__(self):
+        self.client = MongoClient('localhost', 27017)
+        self.db = self.client["otakusenpai"]
+        self.collection = self.db["count_use"]
+
+    def verif_limit(self, user_id):
+        today = datetime.date.today()
+        today_str = today.strftime("%Y-%m-%d")  # Convertir la fecha a una cadena
+        user_key = {"user_id": user_id, "date": today_str}
+        user_record = self.collection.find_one(user_key)
+
+        if user_record is None:
+            user_record = {"user_id": user_id, "date": today_str, "count": 0}
+            self.collection.insert_one(user_record)
+
+        return user_record["count"] < 5
+
+    def reg_use(self, user_id):
+        today = datetime.date.today()
+        today_str = today.strftime("%Y-%m-%d")  # Convertir la fecha a una cadena
+        user_key = {"user_id": user_id, "date": today_str}
+        self.collection.update_one(user_key, {"$inc": {"count": 1}}, upsert=True)
