34e46c897658ca27f933896316023f5e07c288e3
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-02-17 21:55:37 -0500

    Search character upgrade

--- func/anilist/search_character.py
+++ func/anilist/search_character.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
-from func.api_anilist import searchCharacter
+from func.api_anilist import searchCharacter, searchCharacterId
 from deep_translator import GoogleTranslator
 from html2text import html2text
-from telebot.types import ReactionTypeEmoji
+from telebot.types import ReactionTypeEmoji, InlineKeyboardMarkup, InlineKeyboardButton, LinkPreviewOptions
 import telebot
 import requests
 import re
@@ -24,42 +24,50 @@ bot = telebot.TeleBot(Token)
 source_language = 'auto'  # Auto detectar idioma de origen
 target_language = 'es'  # español
 
-def show_character(message):
-    cid = message.chat.id
-    if len(message.text.split(' ')) > 1:
-        print('Haciendo Solicitud a la API')
-        referral_all = message.text.split(" ")
-        character_name = " ".join(referral_all[1:])
-        character = searchCharacter(character_name)
-        if 'errors' in character:
-            reaction = ReactionTypeEmoji(type="emoji", emoji="🤷‍♀")
-            bot.set_message_reaction(message.chat.id, message.message_id, reaction=[reaction])
 
-            for error in character['errors']:
-                match error['message']:
-                    case "Not Found.":
-                        bot.reply_to(message, f"Personaje no encontrado 🙁")
-                    case _:
-                        bot.reply_to(message, error['message'])
+def search_characters(message):
+    if len(message.text.split(' ')) <= 1:
+        bot.reply_to(message, f"Debes poner el nombre del personaje luego de /character")
+        return
+    
+    print('Haciendo Solicitud a la api')
+    referral_all = message.text.split(" ")
+    character_name = " ".join(referral_all[1:])
+    character = searchCharacter(character_name)
+     
+    markup = InlineKeyboardMarkup(row_width=1)
+    btns = []
+    for res in character['data']['Page']['characters'][:min(len(character['data']['Page']['characters']), 8)]:
+        if res['name']['full'] is not None:
+            name = res['name']['full']
         else:
-            reaction = ReactionTypeEmoji(type="emoji", emoji="⚡")
-            bot.set_message_reaction(message.chat.id, message.message_id, reaction=[reaction], is_big=True)
+            name = res['name']['native']
 
-            full_name = character['data']['Character']['name']['full']
-            native_name = character['data']['Character']['name']['native']
-            image = character['data']['Character']['image']['large']
-            description_notr = character['data']['Character']['description'][:500]
-            description_plain_text = html2text(description_notr)
-            description_no_tag = re.sub('<[^<]*', '', description_plain_text)
-            description = GoogleTranslator(source=source_language, target=target_language).translate(description_no_tag)
-            gender = character['data']['Character']['gender']
-            date_of_birth = character['data']['Character']['dateOfBirth']
-            age = character['data']['Character']['age']
-            blood_type = character['data']['Character']['bloodType']
-            is_favorite = character['data']['Character']['isFavourite']
-            site_url = character['data']['Character']['siteUrl']
+        btn = InlineKeyboardButton(str(name), callback_data=f"show_character_{res['id']}")
+        btns.append(btn)
+    markup.add(*btns)
 
-            msg = f"""
+    bot.reply_to(message, 'Estos son los resultados de la busqueda de personajes:', reply_markup=markup)
+
+
+def show_character(chat_id, message_id, id):
+        character = searchCharacterId(id)
+         
+        full_name = character['data']['Character']['name']['full']
+        native_name = character['data']['Character']['name']['native']
+        image = character['data']['Character']['image']['large']
+        description_notr = character['data']['Character']['description'][:500]
+        description_plain_text = html2text(description_notr)
+        description_no_tag = re.sub('<[^<]*', '', description_plain_text)
+        description = GoogleTranslator(source=source_language, target=target_language).translate(description_no_tag)
+        gender = character['data']['Character']['gender']
+        date_of_birth = character['data']['Character']['dateOfBirth']
+        age = character['data']['Character']['age']
+        blood_type = character['data']['Character']['bloodType']
+        is_favorite = character['data']['Character']['isFavourite']
+        site_url = character['data']['Character']['siteUrl']
+         
+        msg = f"""
 <strong>{full_name}</strong> ({native_name})
 <strong>Género:</strong> {gender}
 <strong>Fecha de Nacimiento:</strong> {date_of_birth['year']}-{date_of_birth['month']}-{date_of_birth['day']}
@@ -71,13 +79,10 @@ def show_character(message):
 <strong>Descripción:</strong>
 {description}
 """
-            print(image)
-            res = requests.get(image)
-            with open("./file/" + full_name + ".jpg", 'wb') as out:
-                out.write(res.content)
-            foto = open("./file/" + full_name + ".jpg", "rb")
+                 
+        if image is not None:
+            link_preview_options = LinkPreviewOptions(url=image, prefer_large_media=True, show_above_text=True)
+        else:
+            link_preview_options = LinkPreviewOptions(is_disabled=True)
 
-            #bot.send_message(cid, msg, parse_mode="html")
-            bot.send_photo(cid, foto, msg, parse_mode="html", reply_to_message_id=message.message_id)
-    else:
-        bot.reply_to(message, f"Debes poner el nombre del personaje luego de /character")
+        bot.edit_message_text(msg, chat_id, message_id, parse_mode="html", link_preview_options=link_preview_options)
