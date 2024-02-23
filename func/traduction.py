50a03f7839d0579b61773c3d921b86de800ca7ab
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-02-23 13:31:47 -0500

    Fix traduction

--- func/traduction.py
+++ func/traduction.py
@@ -11,20 +11,23 @@ bot = telebot.TeleBot(Token)
 
 def translate_command(message):
     # Obtener el texto después del comando /tr
-    if message.reply_to_message:
-        text_to_translate = message.reply_to_message.text[4:].strip()
+    if not message.reply_to_message:
+        bot.send_message(message.chat.id, f"Debes hacer reply a un mensaje para poder traducir")
+        return
     
-        # Idiomas de origen y destino
-        source_language = 'auto'  # Auto detectar idioma de origen
-        target_language = 'es'  # español
-    
-        try:
-            # Realizar la traducción
-            translation = GoogleTranslator(source=source_language, target=target_language).translate(text_to_translate)
-    
-            # Enviar la traducción al usuario
-            bot.reply_to(message, f"Traducción 'ES': {translation}")
-    
-        except Exception as e:
-            # Manejar cualquier error durante la traducción
-            bot.reply_to(message, f"Ocurrió un error durante la traducción: {str(e)}")
+    text_to_translate = message.reply_to_message.text
+
+    # Idiomas de origen y destino
+    source_language = 'auto'  # Auto detectar idioma de origen
+    target_language = 'es'  # español
+
+    try:
+        # Realizar la traducción
+        translation = GoogleTranslator(source=source_language, target=target_language).translate(text_to_translate)
+
+        # Enviar la traducción al usuario
+        bot.reply_to(message, f"Traducción 'ES': {translation}")
+
+    except Exception as e:
+        # Manejar cualquier error durante la traducción
+        bot.reply_to(message, f"Ocurrió un error durante la traducción: {str(e)}")
