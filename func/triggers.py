6722292ea03517b2f7f7b21a5462706e3776d821
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-03-23 19:28:46 -0400

    Reparir triggers

--- func/triggers.py
+++ func/triggers.py
@@ -1,9 +1,11 @@
 import time
 import os
 import pickle
+import random
+import re
 
 from database.mongodb import get_db
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from func.list_admins import isAdmin
 from bson import ObjectId
 
@@ -239,3 +241,32 @@ def del_trigger(bot, cid, mid, o_id, uid):
     msg = bot.send_message(cid, f"Trigger eliminado")
     time.sleep(5)
     bot.delete_message(cid, msg.message_id)
+
+def trigger_word(bot, Triggers, message):
+    triggers = {}
+    triggers_equal = {}
+    for doc in Triggers.find():
+        if "eq" in doc:
+            if doc["eq"] is False:
+                triggers_equal[doc["triggers"].lower()] = doc["list_text"]
+            if doc["eq"] is True:
+                triggers[doc["triggers"].lower()] = doc["list_text"]
+     
+    match = False
+    trigger_text = triggers_equal.get(message.text.lower(), None)
+    if len(triggers) != 0:
+        pattern_trigger = re.compile("|".join(triggers))
+        pattern_match = pattern_trigger.search(message.text.lower())
+    else:
+        pattern_match = None
+     
+    if pattern_match:
+        trigger = pattern_match.group()
+        response = random.choice(triggers[trigger])
+        bot.reply_to(message, response)
+     
+    if trigger_text:
+        match = True
+    if match:
+        response = random.choice(trigger_text)
+        bot.reply_to(message, response)
